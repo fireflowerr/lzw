bfb080a73f3e01b1cb19b051f055d22da56da540
paroxayte <tristonchav@gmail.com>
2019-10-24 09:17:31 -0500

    bug fixes. working rev 1

--- src/main/java/encoders/App.java
+++ src/main/java/encoders/App.java
@@ -12,16 +12,16 @@ public final class App
 
     public static void main( String[] args )
     {
-      Path p = Paths.get("test.txt");
-      System.out.println(p.toAbsolutePath().toString());
+      //Path p = Paths.get("test.txt");
+      //System.out.println(p.toAbsolutePath().toString());
 
-      Stream<Character> fIn = null;
-      try {
-        fIn = Streams.readFileChars(p, 256, StandardCharsets.UTF_8);
-      } catch(IOException e) {
-        e.printStackTrace();
-      }
-      if(fIn != null) {
+      //Stream<Character> fIn = null;
+      //try {
+      //  fIn = Streams.readFileChars(p, 256, StandardCharsets.UTF_8);
+      //} catch(IOException e) {
+      //  e.printStackTrace();
+      //}
+      //if(fIn != null) {
       //System.out.println(fIn.findFirst().get());
       //  String result = fIn .collect(Collector.of(
       //      StringBuilder::new
@@ -30,7 +30,19 @@ public final class App
       //    , StringBuilder::toString));
 
       //  System.out.println(result);
-      }
+      String test = "meowmeeeoooowmeowmowmeoowmoo";
+      BidiDict<Pair<Character, Integer>, Integer> dict = new Dict<>();
+      int lzwBot = Lzw.getLzwBot();
+      dict.add(new Pair<>('m', lzwBot), 0);
+      dict.add(new Pair<>('e', lzwBot), 1);
+      dict.add(new Pair<>('o', lzwBot), 2);
+      dict.add(new Pair<>('w', lzwBot), 3);
+      BidiDict<Integer, Pair<Character, Integer>> iDict = dict.flip();
+
+      Stream<Character> in = test.chars().mapToObj(x -> Character.valueOf((char)x));
+      Lzw.decode(iDict, Lzw.encode(dict, in))
+          .forEach(System.out::print);
+      System.out.println();
     }
 
     //out
